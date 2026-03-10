299fc8904d1c3c697317c90fee1a565d19cbfe08
Samption <sxl5331257@163.com>
2026-03-11 01:00:34 +0800

    开发AI模块 1.开发AI代码生成、代码解析、代码保存 2.使用策略、执行器、模板模式重构代码

---
+++ src/main/java/com/sxl/nocode/ai/AiCodeGeneratorService.java
@@ -0,0 +1,49 @@
+package com.sxl.nocode.ai;
+
+import com.sxl.nocode.ai.model.HtmlCodeResult;
+import com.sxl.nocode.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
+
+public interface AiCodeGeneratorService {
+
+    String generateCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    HtmlCodeResult generateHtmlCode(String userMessage);
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码（流式，响应式编程，返回 Flux 对象）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码（流式）
+     *
+     * @param userMessage 用户消息
+     * @return 生成的代码结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
+}
+
