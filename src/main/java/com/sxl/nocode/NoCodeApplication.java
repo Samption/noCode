eeb8eba82659ff982055b2d7dcdf0db9a960b4de
Samption <sxl5331257@163.com>
2026-04-18 17:01:19 +0800

    缓存优化，使用redis缓存精选案例

--- src/main/java/com/sxl/nocode/NoCodeApplication.java
+++ src/main/java/com/sxl/nocode/NoCodeApplication.java
@@ -4,11 +4,13 @@ import dev.langchain4j.community.store.embedding.redis.spring.RedisEmbeddingStor
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @SpringBootApplication(exclude = {RedisEmbeddingStoreAutoConfiguration.class})
 @EnableAspectJAutoProxy
 @MapperScan("com.sxl.nocode.mapper")
+@EnableCaching
 public class NoCodeApplication {
     public static void main(String[] args) {
         SpringApplication.run(NoCodeApplication.class, args);
